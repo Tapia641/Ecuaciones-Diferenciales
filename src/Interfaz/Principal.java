42d33e27ed2cec8183b54ffdf3e1cc79186600c5
tapia641 <tapia641@gmail.com>
2017-05-01 09:57:13 -0500

    se ajusta el tamanio

--- src/Interfaz/Principal.java
+++ src/Interfaz/Principal.java
@@ -5,6 +5,8 @@
  */
 package interfaz;
 
+import javax.swing.JDialog;
+
 /**
  *
  * @author Tapia
@@ -27,11 +29,28 @@ public class Principal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        JDialogNuevaEcuacion = new javax.swing.JDialog();
+        jLabel2 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem2 = new javax.swing.JMenuItem();
         jMenuItem1 = new javax.swing.JMenuItem();
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/ecuacion.jpg"))); // NOI18N
+
+        javax.swing.GroupLayout JDialogNuevaEcuacionLayout = new javax.swing.GroupLayout(JDialogNuevaEcuacion.getContentPane());
+        JDialogNuevaEcuacion.getContentPane().setLayout(JDialogNuevaEcuacionLayout);
+        JDialogNuevaEcuacionLayout.setHorizontalGroup(
+            JDialogNuevaEcuacionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        JDialogNuevaEcuacionLayout.setVerticalGroup(
+            JDialogNuevaEcuacionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, JDialogNuevaEcuacionLayout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jLabel2))
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -40,6 +59,11 @@ public class Principal extends javax.swing.JFrame {
         jMenu1.setText("Archivo");
 
         jMenuItem2.setText("Nuevo");
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
         jMenu1.add(jMenuItem2);
 
         jMenuItem1.setText("Salir");
@@ -65,9 +89,18 @@ public class Principal extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+        // TODO add your handling code here:
+        JDialogNuevaEcuacion.setLocationRelativeTo(null);
+        JDialogNuevaEcuacion.setBounds(0, 0, 500, 500);
+        JDialogNuevaEcuacion.setVisible(true);
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JDialog JDialogNuevaEcuacion;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
