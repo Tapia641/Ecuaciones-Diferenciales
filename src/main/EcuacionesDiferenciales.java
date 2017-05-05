244e4c9c61dbe69efa1cd38a6187b03de89a8648
tapia641 <tapia641@gmail.com>
2017-05-05 16:48:26 -0500

    Revert "Se ajusta el tamanio de la interfaz y el fondo"
    This reverts commit 2a699e049d63900cb70e4911ea31a00564f0d1d9.

--- src/main/EcuacionesDiferenciales.java
+++ src/main/EcuacionesDiferenciales.java
@@ -1,16 +1,10 @@
 package main;
-
-import interfaz.Principal;
-
 /**
  *
  * @author HERNANDEZ TAPIA LUIS ENRIQUE 
  */
 public class EcuacionesDiferenciales {
     public static void main(String[] args) {
-        Principal t=new Principal();
-        t.setVisible(true);
-        t.setLocationRelativeTo(null);
     }
     
 }
